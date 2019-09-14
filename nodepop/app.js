44b0303f24ef4a98ca5ced8ec0c83910a2644652
Richard Palacios G <apalaciosg91@gmail.com>
2019-09-14 17:53:31 -0500

    Agrego conexion BBDD para que arranque con la applicacion en app.js

--- nodepop/app.js
+++ nodepop/app.js
@@ -4,23 +4,32 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/**
+ * Conexión a la base de datos
+ */
+require('./lib/connectDB');
+
+// Variables goblales para vistas
+app.locals.title = 'NodePop';
+
+/**
+ * Rutas de mi aplicación web
+ */
+app.use('/',      require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
