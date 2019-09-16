09fe90bdf77f9919ed0488f33ad944841ac9c5ab
Richard Palacios G <apalaciosg91@gmail.com>
2019-09-16 02:20:53 -0500

    Mofico error handler

--- nodepop/app.js
+++ nodepop/app.js
@@ -23,6 +23,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 require('./lib/connectDB');
 require('./models/Anuncio');
 
+/**
+ * Rutas de mi API
+ */
+app.use('/api/anuncios', require('./routes/api/anuncios'));
+
 // Variables goblales para vistas
 app.locals.title = 'NodePop';
 
@@ -39,13 +44,34 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  // comprueba error de validaciòn
+  if (err.array) {
+    err.status = 422;
+    const errInfo = err.array({ onlyFirstError: true })[0];
+    err.message = isAPI(req) 
+    ? {message: 'Not valid', errors: err.mapped()}
+    : `Not valid - ${errInfo.param} ${errInfo.msg}`;
+  }
+
+  res.status(err.status || 500);
+  
+  if(isAPI(req)) {
+    res.json({ success: false, error: err.message});
+    return;
+  }
+  
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  
+  
   // render the error page
-  res.status(err.status || 500);
   res.render('error');
 });
+
+// funcion para saber si es una petición a un API
+function isAPI(req) {
+  return req.originalUrl.indexOf('/api') === 0;
+}
 
 module.exports = app;
