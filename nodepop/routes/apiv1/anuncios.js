5908b8d967459184f0d2fddba10e1dcd40a1a1df
Richard Palacios G <apalaciosg91@gmail.com>
2019-09-24 05:02:29 -0500

    Agrego recurso consultar anuncio por id
    Y cambio variable de query param de skip a start .

--- nodepop/routes/apiv1/anuncios.js
+++ nodepop/routes/apiv1/anuncios.js
@@ -7,17 +7,34 @@ const anuncioController = require('../../controllers/AnuncioController');
 const Anuncio = require('../../models/Anuncio');
 
 /**
- * GET /apiv1/anuncios
+ * GET /anuncios
  * Devuelve una lista de anuncios
  * http://localhost:3000/apiv1/anuncios
  */
 router.get('/', async (req, res, next) => {
     try {
-        const skip = parseInt(req.query.skip);
+        const skip = parseInt(req.query.start);
         const limit = parseInt(req.query.limit);
         const anuncios = await anuncioController.listaAnuncios({skip, limit});
-        res.json({ success: true, anuncios });
+        res.json({ success: true, results: anuncios });
     } catch(err) {
+        next(err);
+    }
+});
+
+/**
+ * Get /anuncios/:id
+ * Obtiene un agente mediante el :id
+ */
+router.get('/:id', async(req, res, next) => {
+    try {
+        const _id = req.params.id;        
+        const anuncio = await anuncioController.consultarUnAnuncio(_id);        
+        if(!anuncio) {
+            res.status(404).json({success: false});
+        }
+        res.json({success: true, result: anuncio});
+    } catch (error) {
         next(err);
     }
 });
