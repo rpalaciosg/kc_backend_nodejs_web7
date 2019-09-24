fbd6142a7da446260772cea09cec3ce1be41bc49
Richard Palacios G <apalaciosg91@gmail.com>
2019-09-24 05:00:46 -0500

    Agrega controlador al app.js

--- nodepop/app.js
+++ nodepop/app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
  */
 require('./lib/connectDB');
 require('./models/Anuncio');
+require('./controllers/AnuncioController');
 
 /**
  * Rutas de mi API
