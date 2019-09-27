7379c471b0b10e8680158997667416d7fe7c68c2
Richard Palacios G <apalaciosg91@gmail.com>
2019-09-27 05:42:28 -0500

    agrego iamgen thumbnail y corrijo documentacion

--- nodepop/lib/connectDB.js
+++ nodepop/lib/connectDB.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 
 // cargar librería de mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const conn = mongoose.connection;
 
 // gestionar eventos de conexion
-conn.on('error', err => {
-    console.log('Error de conexiòn', err);
+conn.on("error", err => {
+    console.log("Error de conexiòn", err);
     process.exit(1);
 });
 
-conn.once('open', () =>  {
-    console.log('Conectado a MongoDB en', conn.name);
+conn.once("open", () =>  {
+    console.log("Conectado a MongoDB en", conn.name);
 });
 
 // conectar
-mongoose.connect('mongodb://localhost/nodepop', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost/nodepop", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 // exportar la conexiòn
 module.exports = conn;
