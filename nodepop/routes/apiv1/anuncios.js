39e2a50570bba1b3a812ae4fbcee034bf21496c1
Richard Palacios G <apalaciosg91@gmail.com>
2019-09-29 05:06:54 -0500

    Agrego filtro para el precio usando regex

--- nodepop/routes/apiv1/anuncios.js
+++ nodepop/routes/apiv1/anuncios.js
@@ -17,26 +17,62 @@ router.get('/', async (req, res, next) => {
         const limit = parseInt(req.query.limit);
         const fields = req.query.fields;
         const sort = req.query.sort;
-        const tags = req.query.tags;
+        const tags = req.query.tags || "";
         const venta = req.query.venta;
-        //const precio // para el filtro por precio debo hacer un typeof para comparar que no sea 'undefined'
+        const precio = req.query.precio;
+        const extTags = tags.split(" ");
+        const extPrecio = precio.split("-");
+        console.log("extraigo precio---->",extPrecio);
+        let patPreIgual = /\d/;
+        let patPreMenor = /-\d/;
+        let patPreEntre = /\d-\d/;
+        let patPreMayor = /\d-/;
+        
         const filter = {};
         
         // para este filtro por tags lo debo hacer con un $in
         // const cursor = db.collection('anuncios').find({ tags: { $in: ['work','stylelife']}});
-        if (tags) { 
-            filter.tags = tags;
+        if (tags !== "") { 
+            if (extTags.length > 1) { 
+
+            } else {
+                filter.tags = tags;
+            }
+            
         }
 
         if (venta) {
             filter.venta = venta;
         }
+
+        //const precio // para el filtro por precio debo hacer un typeof para comparar que no sea 'undefined'
+        if (typeof precio !== 'undefined') {
+            if (patPreIgual.test(precio)) {
+                filter.precio = extPrecio[0];
+            }
+            
+            if (patPreMenor.test(precio)) {
+                console.log("arma query menor---->", { '$lte': parseInt(extPrecio[1]) });
+                filter.precio = { '$lte': parseInt(extPrecio[1]) };
+                
+            }
+             if (patPreEntre.test(precio)) {
+                filter.precio = { '$gte': extPrecio[0], '$lte': extPrecio[1] };
+            } 
+            
+            if (patPreMayor.test(precio)) {
+                filter.precio = { '$gte': extPrecio[0] };
+            }
+        }
+
         const anuncios = await anuncioController.listaAnuncios({filter:filter, skip, limit, fields, sort});
         res.json({ success: true, results: anuncios });
     } catch(err) {
         next(err);
     }
 });
+
+
 
 /**
  * GET /anuncios/:id
